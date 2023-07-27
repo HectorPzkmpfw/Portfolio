21cd294937b853054c00a8532a0c15ede0987a98
HectorPzkmpfw <maciashector00@gmail.com>
2023-07-26 21:04:16 -0500

    interactive code done, adding responsive styling

--- script/script.js
+++ script/script.js
@@ -1,7 +1,31 @@
-const hectorPicture = document.getElementById("banImg")
+document.getElementById("hire").addEventListener("click", myFunction);
 
-function hover(hectorPicture){
-    
+function myFunction() {
+  document.getElementById("hire").innerHTML = "Thanks!";
 }
 
-hectorPicture.addEventListener("mouseover", changePicture);
+document.getElementById('select').addEventListener("mouseover", showProjects);
+document.getElementById('select').addEventListener("mouseout", hideProjects);
+document.getElementById('work').addEventListener("mouseover", showProjects);
+document.getElementById('work').addEventListener("mouseout", hideProjects);
+document.getElementById('work').addEventListener("mouseover", projectHover);
+document.getElementById('work').addEventListener("mouseout", projectMouseOut);
+
+function projectHover() {
+  document.getElementById("select").style.backgroundColor = "rgb(50, 119, 247)";
+  document.getElementById("select").style.color = "white";
+  document.getElementById("select").style.borderRadius = "5px";
+}
+
+function projectMouseOut(){
+  document.getElementById("select").style.backgroundColor = "white";
+  document.getElementById("select").style.color = "black";
+}
+
+function showProjects() {
+    document.getElementById('work').style.visibility = "visible";
+}
+
+function hideProjects() {
+    document.getElementById('work').style.visibility = "hidden";
+}
